9417aab168cee1a00fa4cde0ad97212ed8d61da6
SumanBanerjee12 <96712919+SumanBanerjee12@users.noreply.github.com>
2025-08-18 23:34:37 +0530

    Commit Duplicate

--- src/practies/Duplicate_Using_Map.java
+++ src/practies/Duplicate_Using_Map.java
@@ -25,21 +25,4 @@ public class Duplicate_Using_Map {
 		}
 	}
 }
-/*
-public class Duplicate_Using_Map {
-	public static void main(String[] args) {
-		
-		String str = "mynameissuman";
-		
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		for(char ch : str.toCharArray()) {
-			map.put(ch, map.getOrDefault(ch, 0) + 1);
-		}
-		
-		for(Map.Entry<Character, Integer> entry : map.entrySet()) {
-			if(entry.getValue( )> 1) {
-				System.out.println(entry.getKey() + "="+ entry.getValue());
-			}
-		}
-	}
-}*/
+
