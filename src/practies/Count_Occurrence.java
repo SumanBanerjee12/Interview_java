046ae8737630cb5ecfcf6c1dc7292305f0c6bb2f
SumanBanerjee12 <96712919+SumanBanerjee12@users.noreply.github.com>
2025-08-18 23:30:42 +0530

    Commit count occurrence

--- src/practies/Count_Occurrence.java
+++ src/practies/Count_Occurrence.java
@@ -1,7 +1,7 @@
 package practies;
 
 //Count occurrence of a given character in a string using Stream API in Java.
-/*
+
 public class Count_Occurrence {
 
 	public static void main(String[] args) {
@@ -14,19 +14,9 @@ public class Count_Occurrence {
 		System.out.println(count);
 	}
 	
-}*/
-
-
-public class Count_Occurrence{
-
-	public static void main(String[] args) {
-		
-		String str = "geeksforgeeks";
-		char ch = 'g';
-		
-		long count = str.chars().filter(c -> c == ch).count();
-		System.out.println(count);
-	}
 }
+
+
+
 
 
